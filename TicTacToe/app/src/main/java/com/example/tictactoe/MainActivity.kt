92f28f0ae516a3f6b6876fa47ef6698765ca6e59
Benjamin Barreto <benalexb@gmail.com>
2023-02-11 17:10:53 +0100

    We can create a grid, yay, but no user interaction yet

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.tictactoe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +31,59 @@ class MainActivity : ComponentActivity() {
             TicTacToeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LazyVerticalGridDemo()
                 }
             }
         }
     }
 }
 
+val state = listOf<String>(
+    "X", "O", "",
+    "X", "O", "X",
+    "", "", "X"
+)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LazyVerticalGridDemo(){
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(128.dp),
+        modifier = Modifier.background(Color.Black),
+        // content padding
+        contentPadding = PaddingValues(
+            start = 12.dp,
+            top = 16.dp,
+            end = 12.dp,
+            bottom = 16.dp
+        ),
+        content = {
+            items(state.size) { index ->
+                Card(
+                    backgroundColor = Color.White,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth(),
+                    elevation = 8.dp,
+                ) {
+                    val textContent = state[index]
+                    Text(
+                        text = textContent,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TicTacToeTheme {
-        Greeting("Android")
+        LazyVerticalGridDemo()
     }
 }
