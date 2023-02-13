0d74a4d5a7d8d142aa3cca6e6f5139f65c872adb
Benjamin Barreto <benjamin.barreto@shopify.com>
2023-02-13 10:49:10 +0100

    Yay we can keep a mutable list state now!

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,61 +27,79 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TicTacToeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    LazyVerticalGridDemo()
-                }
+                TicTacToeGame()
             }
         }
     }
 }
 
-val state = listOf<String>(
-    "X", "O", "",
-    "X", "O", "X",
-    "", "", "X"
-)
-
+@Preview()
 @Composable
-fun LazyVerticalGridDemo(){
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(128.dp),
-        modifier = Modifier.background(Color.Black),
-        // content padding
-        contentPadding = PaddingValues(
-            start = 12.dp,
-            top = 16.dp,
-            end = 12.dp,
-            bottom = 16.dp
-        ),
-        content = {
-            items(state.size) { index ->
-                Card(
-                    backgroundColor = Color.White,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxWidth(),
-                    elevation = 8.dp,
-                ) {
-                    val textContent = state[index]
-                    Text(
-                        text = textContent,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 30.sp,
-                        color = Color.Black,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.padding(16.dp)
-                    )
-                }
-            }
-        }
-    )
+fun TicTacToeGame() {
+    TicTacToeTheme {
+        TicTacToeLayout(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun DefaultPreview() {
-    TicTacToeTheme {
-        LazyVerticalGridDemo()
+fun TicTacToeLayout(modifier: Modifier = Modifier){
+    var gameState = remember {
+        mutableStateListOf<String>(
+            "X", "", "",
+            "O", "", "",
+            "", "", "X",
+        )
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(128.dp),
+            modifier = Modifier.background(Color.Black),
+            // content padding
+            contentPadding = PaddingValues(
+                start = 12.dp,
+                top = 16.dp,
+                end = 12.dp,
+                bottom = 16.dp
+            ),
+            content = {
+                items(gameState.size) { index ->
+                    Card(
+                        backgroundColor = Color.White,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth(),
+                        elevation = 8.dp,
+                        onClick = {
+                            println("TEST! Card Clicked!")
+                        }
+                    ) {
+                        Text(
+                            text = gameState[index],
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+        Button(onClick = {
+            for (i in gameState.indices) {
+                gameState[i] = ""
+            }
+        }) {
+            Text(text = stringResource(R.string.reset_button))
+        }
     }
 }
